93a69fad4fcf22d60747996c4a5ccc0885710acb
Monolocker <Gem.trigg69@yahoo.com>
2026-03-03 13:39:52 -0600

    limit framerate to 60 fps + frame time-tracking

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ from constants import SCREEN_HEIGHT
 from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # Delta time
+
+    # Start infinite game loop 
     while True:
         log_state()
+        
+        # Event handling, checks for user interactions since last frame
         for event in pygame.event.get():
+            # Exit loop if window is closed
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
-        pygame.display.flip()
+        pygame.display.flip() # Refresh screen to show updates
+
+        # Limit framerate to 60 FPS
+        clock.tick(60) 
+        dt = clock.tick(60) / 1000 # Convert from ms to s 
 
 if __name__ == "__main__":
     main()
