3579524ab5f6f116315b16c9f3880254a42c796e
Monolocker <Gem.trigg69@yahoo.com>
2026-03-03 14:32:18 -0600

    Add player class and draw method to main loop

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import pygame
-from constants import SCREEN_WIDTH
-from constants import SCREEN_HEIGHT
+from constants import * 
 from logger import log_state
+from circleshape import CircleShape
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # Instantiate player object
     dt = 0 # Delta time
 
     # Start infinite game loop 
     while True:
         log_state()
         
-        # Event handling, checks for user interactions since last frame
+        # Event handling
         for event in pygame.event.get():
             # Exit loop if window is closed
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen) # Re-render player on screen each frame
         pygame.display.flip() # Refresh screen to show updates
 
-        # Limit framerate to 60 FPS
-        clock.tick(60) 
-        dt = clock.tick(60) / 1000 # Convert from ms to s 
+        # Limit framerate to 60 FPS, convert ms to s
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
