e9517264de2d701205cd0eee14e46057a42c7cb3
Monolocker <Gem.trigg69@yahoo.com>
2026-03-05 14:52:37 -0600

    add asteroid class and sprite group setup

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ from constants import *
 from logger import log_state
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    dt = 0
 
     # Create groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     # Add Player class to groups before player object instance creation
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # Instantiate player object
 
-    dt = 0
+    # Add asteroid to groups
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    # Create AsteroidField object
+    AsteroidField.containers = (updatable, )
+    asteroid_field = AsteroidField()
+
 
     # Start infinite game loop 
     while True:
